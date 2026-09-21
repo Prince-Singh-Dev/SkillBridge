a3c130623b0f062c04f4b171d6f64b7307cd2f38
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-09-21 13:55:43 +0000

    connected database with server

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
 require("dotenv").config() // method to load environment variables from .env file
 const app = require("./src/app")
+const connectToDB = require("./src/config/database")
+
+
+connectToDB()
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
