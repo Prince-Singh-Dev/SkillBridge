ed917d3064cbd528f23f2bb2f5bba267a2c86ebe
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-09-21 14:13:00 +0000

    created userModel and setting up routes and controllers

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+const userModel = require("../models/user.model")
+
+/* 
+    @route POST /api/auth/register
+    @description Register a new user
+    @access Public
+*/
+
+async function reqisterUserController(req,res){
+
+}
