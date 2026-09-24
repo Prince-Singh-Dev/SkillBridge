86efbb0cde4b318bce5bdfd084d1a9da9ace5a6f
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-09-24 19:07:25 +0530

    created the getme data route

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -33,6 +33,6 @@ authRouter.get("/logout",authController.logoutUserController)
  * @access Private
  */
 
-authRouter.get("/get-me",authMiddleware.authUser,)
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController)
 
 module.exports = authRouter
